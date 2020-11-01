c0849e0c91bf348f4398bf80784d2a90807d2a2c
MaccKOT <51321216+MaccKOT@users.noreply.github.com>
2020-11-01 13:54:23 +0400

    Navigation and routing handling

--- pages/about/index.js
+++ pages/about/index.js
@@ -1,3 +1,22 @@
+import Router from 'next/router';
+
 export default function About() {
-  return <h1>About page</h1>;
+  const linkClickHandler = () => {
+    Router.push('/'); // https://nextjs.org/docs/api-reference/next/router#userouter
+  };
+
+  return (
+    <>
+      <h1>About page</h1>
+
+      <button onClick={linkClickHandler}>Back to home page</button>
+      <button
+        onClick={() => {
+          Router.push('/posts');
+        }}
+      >
+        Go to posts page
+      </button>
+    </>
+  );
 }
--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,19 @@
 import styles from '../styles/Home.module.css';
+import Link from 'next/link'; //для полноценного роутинга как в SPA нам необходимо все ссылки пропускать через специальный компонент
 
 export default function Home() {
   return (
     <div className={styles.container}>
       <p className={styles.title}>Hello, Next.js</p>
-      <a href="/about">See about page</a>
-      <a href="/posts">See posts page</a>
-      <a href="/post/25">Some post page with id</a>
+      <Link href={'/about'}>
+        <a>See about page</a>
+      </Link>
+      <Link href="/posts">
+        <a>See posts page</a>
+      </Link>
+      <Link href="/post/25">
+        <a>Some post page with id</a>
+      </Link>
     </div>
   );
 }
