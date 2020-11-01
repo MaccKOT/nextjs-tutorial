48d6882eaaa01415a71c707de2b9437f7ffcaafc
MaccKOT <51321216+MaccKOT@users.noreply.github.com>
2020-11-01 14:08:08 +0400

    Work with head section

--- pages/index.js
+++ pages/index.js
@@ -1,19 +1,29 @@
 import styles from '../styles/Home.module.css';
 import Link from 'next/link'; //для полноценного роутинга как в SPA нам необходимо все ссылки пропускать через специальный компонент
+import Head from 'next/head'; //компонент для работы с метатегами и заголовками. Так как в next нету страницы index.html, то вся работа ведётся через этот компонент
 
 export default function Home() {
   return (
-    <div className={styles.container}>
-      <p className={styles.title}>Hello, Next.js</p>
-      <Link href={'/about'}>
-        <a>See about page</a>
-      </Link>
-      <Link href="/posts">
-        <a>See posts page</a>
-      </Link>
-      <Link href="/post/25">
-        <a>Some post page with id</a>
-      </Link>
-    </div>
+    <>
+      <Head>
+        <meta charSet="UTF-8" />
+        <meta name="keywords" content="HTML, CSS, JavaScript" />
+        <meta name="description" content="Free Web tutorials" />
+        <title>Next.js App</title>
+      </Head>
+
+      <div className={styles.container}>
+        <p className={styles.title}>Hello, Next.js</p>
+        <Link href={'/about'}>
+          <a>See about page</a>
+        </Link>
+        <Link href="/posts">
+          <a>See posts page</a>
+        </Link>
+        <Link href="/post/25">
+          <a>Some post page with id</a>
+        </Link>
+      </div>
+    </>
   );
 }
--- pages/posts.js
+++ pages/posts.js
@@ -1,5 +1,14 @@
+import Head from 'next/head';
+
 export default function Posts() {
-  return <h1>Post's page</h1>;
+  return (
+    <>
+      <Head>
+        <title>Posts Page | Next.js course</title>
+      </Head>
+      <h1>Post's page</h1>
+    </>
+  );
 }
 
 //posts
