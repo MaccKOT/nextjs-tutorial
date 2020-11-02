5ecef40144f6019211437b7da026529110d5c8b6
MaccKOT <51321216+MaccKOT@users.noreply.github.com>
2020-11-02 11:24:42 +0400

    Mockup json server and posts fetch component

--- pages/posts.js
+++ pages/posts.js
@@ -1,9 +1,25 @@
+import { useState, useEffect } from 'react';
 import MainLayout from '../layouts/MainLayout';
 
 export default function Posts() {
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    async function loadPosts() {
+      const response = await fetch('http://localhost:4200/posts');
+      const json = await response.json();
+      setPosts(json);
+      // console.log(json);
+    }
+
+    loadPosts();
+  }, []);
+
   return (
     <MainLayout title="Posts Page">
       <h1>Post's page</h1>
+
+      <pre>{JSON.stringify(posts)}</pre>
     </MainLayout>
   );
 }
