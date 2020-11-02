bb1141db1b912c76e45f69d1fda6103d2c9ba878
MaccKOT <51321216+MaccKOT@users.noreply.github.com>
2020-11-02 12:53:24 +0400

    Nextjs-progressbar

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,20 @@
 import '../styles/globals.css';
+import NextNprogress from 'nextjs-progressbar';
 
 //компонент для использования глобальных стилей
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <NextNprogress
+        color="#778899"
+        startPosition={0.3}
+        stopDelayMs={200}
+        height="2"
+      />
+      <Component {...pageProps} />
+    </>
+  );
 }
 
 export default MyApp;
